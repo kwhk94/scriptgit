7bffe6bfe1afc85f51e275cfd0bdf20baf266612
wjstkdgns93 <wjstkdgns93@naver.com>
2016-05-22 11:07:27 +0900

    xml.etree 적용
    적용 끝

--- script.py
+++ script.py
@@ -1,5 +1,15 @@
 import http.client
-from xml.dom.minidom import *
+from xml.etree import ElementTree
+
+def extractBookData(strXml):
+    tree = ElementTree.fromstring(strXml)
+    # Book 엘리먼트를 가져옵니다.
+    itemElements = tree.getiterator("item")  # return list type
+    for item in itemElements:
+        strTitle = item.find("yadmNm")
+        if len(strTitle.text) > 0 :
+            print("yadmNm : ",strTitle.text)
+
 
 conn = http.client.HTTPConnection("openapi.hira.or.kr")
 
@@ -9,11 +19,16 @@ conn.request("GET","/openapi/service/hospInfoService/getHospBasisList?" \
              +servicekey)
 
 reg = conn.getresponse()
-
+if int(reg.status) == 200:
+    print("Book data downloading complete!")
+    extractBookData(reg.read())
+else:
+    print("OpenAPI request has been failed!! please retry")
+"""
 cLen = reg.getheader("Content-Length")
 
 xmlsrc = reg.read(cLen).decode("utf-8")
+"""
 
-doc = parseString(xmlsrc)
 
 
