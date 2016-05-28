5298ed1ea18537856875fcaba4dfabc6af254045
kwhk94 <clsrndi13>
2016-05-28 16:05:27 +0900

    병원이름검색

--- script http.py
+++ script http.py
@@ -19,8 +19,9 @@ BooksDoc = None
 def printMenu():
     print("\nWelcome! hospital Manager Program (http version)") 
     print("========Menu==========")
-    print("Load http:  l")
-    print("Print dom to xml: p")
+    print("시에대한 HTTP 불러오기:  l")
+    print("찾을 병원 이름 : h")
+    print("Print: p")
     print("Quit program:   q")
     print("print Book list: b")
     print("sEarch Book Title: e")
@@ -34,6 +35,8 @@ def launcherFunction(menu):
         LoadXMLFromHTTP()  
     elif menu == 'q':
         QuitBookMgr()
+    elif menu=='h':
+        LoadHospitalName()
     elif menu == 'p':     
         extractBookData(HospitalDoc.read().decode('utf-8'))
 #    elif menu == 'b':
@@ -45,7 +48,16 @@ def launcherFunction(menu):
     else:
         print ("error : unknow menu key")
 
-
+def LoadHospitalName():
+    global HospitalDoc, Code, sidoName,Hname
+    conn = HTTPConnection("openapi.hira.or.kr")
+    Hospital = quote(input("please input hospital name to load :"))
+    Hname = "yadmNm=" + Hospital + "&"  # 읽어올 파일경로를 입력 받습니다.
+    print(sidoName      ,  Hname)
+    conn.request("GET","/openapi/service/hospInfoService/getHospBasisList?" + sidoName+ Hname+"numOfRows=1000&ServiceKey=Id4vjBVQEtf9S3cDoQcUnmSSidJLPlzQIflfPq2Nr2n6CTK5OBvtYqDU3T0skasLZybrxivIfIXiNXRs1%2Bhdlg%3D%3D")
+    req = conn.getresponse()
+    HospitalDoc = req
+    return None
 
 def connectOpenAPIServer():
     global conn, server
@@ -59,34 +71,47 @@ def extractBookData(strXml):
     # Book 엘리먼트를 가져옵니다.
     itemElements = tree.getiterator("item")  # return list type
     for item in itemElements:
-        strTitle = item.find("sidoCdNm")
-        if len(strTitle.text) > 0 :
-            print("sidoNm : ",strTitle.text)
+        sidoTitle = item.find("sidoCdNm")
+        yadmNmTitle=item.find("yadmNm")
+        if len(sidoTitle.text) > 0 :
+            print("시,도 위치 : ",sidoTitle.text)
+        if len(yadmNmTitle.text)>0:
+            print("병원이름 : ",yadmNmTitle.text)
+    ReLoadHTTP()
    
 
 
 def LoadXMLFromHTTP():
-    global HospitalDoc,Code
+    global HospitalDoc,Code,sidoName
     Code={'서울':'110000','부산':'210000','인천':'220000'
     ,'대구':'230000','경남':'380000','경기':'310000',
     '충남':'340000','강원':'320000','전북':'350000','광주':'240000'
     ,'충북':'330000','울산':'260000','전남':'360000','대전':'250000'
     ,'경북':'370000','제주':'390000','세종시':'410000'}
+
     conn = HTTPConnection("openapi.hira.or.kr")
     sido=str(input ("please input sido name to load :"))
-    print(Code[sido])
     sidoName ="sidoCd="+ Code[sido]+"&"  # 읽어올 파일경로를 입력 받습니다.
-    sgguName = "sgguCd="+quote(input ("please input sggu name to load :"))+"&"  # 읽어올 파일경로를 입력 받습니다.
-    print(quote(sidoName))
-    conn.request("GET", "/openapi/service/hospInfoService/getHospBasisList?"+sidoName+sgguName+"numOfRows=1000&ServiceKey=Id4vjBVQEtf9S3cDoQcUnmSSidJLPlzQIflfPq2Nr2n6CTK5OBvtYqDU3T0skasLZybrxivIfIXiNXRs1%2Bhdlg%3D%3D") 
+    conn.request("GET", "/openapi/service/hospInfoService/getHospBasisList?"+sidoName+"numOfRows=1000&ServiceKey=Id4vjBVQEtf9S3cDoQcUnmSSidJLPlzQIflfPq2Nr2n6CTK5OBvtYqDU3T0skasLZybrxivIfIXiNXRs1%2Bhdlg%3D%3D")
     req = conn.getresponse()   
     print(req.status,req.reason)
     HospitalDoc=req
-    return None   
+    return None
+
+def ReLoadHTTP():
+    global HospitalDoc, Code, sidoName,Hname
+    conn = HTTPConnection("openapi.hira.or.kr")
+    if conn!=None:
+        conn.request("GET","/openapi/service/hospInfoService/getHospBasisList?" + sidoName+ Hname + "numOfRows=1000&ServiceKey=Id4vjBVQEtf9S3cDoQcUnmSSidJLPlzQIflfPq2Nr2n6CTK5OBvtYqDU3T0skasLZybrxivIfIXiNXRs1%2Bhdlg%3D%3D")
+        req = conn.getresponse()
+        print(req.status, req.reason)
+        HospitalDoc = req
+        return None
+    else :
+        print("시에대한 정보가 없습니다.")
 
 
 
-    
 
 def QuitBookMgr():
     global loopFlag
