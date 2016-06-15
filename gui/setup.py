ec06dea0dc14311dc88094965aea22d2630a2e81
wjstkdgns93 <wjstkdgns93@naver.com>
2016-06-15 20:59:08 +0900

    sdist
    적용끝

--- gui/setup.py
+++ gui/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='book',
+setup(name='Hospital',
       version='1.0',
       py_modules=['GUItest','hosdata','hospital','mail','near','script http','search']
       )
